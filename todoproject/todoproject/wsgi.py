dd2578ab0e3800a5780b7e7ef20b2b6569c5b963
Ahmad Zaki Haninut Tuqo <zakiahmad857@gmail.com>
2020-10-22 02:52:31 +0700

    s

--- todoproject/todoproject/wsgi.py
+++ todoproject/todoproject/wsgi.py
@@ -1,16 +1,10 @@
-"""
-WSGI config for todoproject project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
-"""
-
 import os
-
+from django.conf import settings
+from django.contrib.staticfiles.handlers import StaticFilesHandler
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'todoproject.settings')
-
-application = get_wsgi_application()
+if settings.DEBUG:
+    application = StaticFilesHandler(get_wsgi_application())
+else:
+    application = get_wsgi_application()
