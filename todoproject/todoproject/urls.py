bb9f5f06d47b7688d4dfd81cdc2e7f4b2437ef26
Ahmad Zaki Haninut Tuqo <zakiahmad857@gmail.com>
2020-10-22 02:36:12 +0700

    as

--- todoproject/todoproject/urls.py
+++ todoproject/todoproject/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import include
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('user/', include('user.urls')),
