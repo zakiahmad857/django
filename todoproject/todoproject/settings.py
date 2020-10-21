44bf599527b04f013e1f520a235f1cf2e25dd391
Ahmad Zaki Haninut Tuqo <zakiahmad857@gmail.com>
2020-10-22 00:01:54 +0700

    Update settings.py

--- todoproject/todoproject/settings.py
+++ todoproject/todoproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '8xftu=jt49adv(rnqa)vi1lmnb82zxys6h2gc0cxyh-fti6w$u'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", "localhost","ahmadzakiht.com", "www.ahmadzakiht.com"]
 
 
 # Application definition
